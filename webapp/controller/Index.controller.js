0b5a394f2a539309bb2e289526e3dab61038e129
Walter Enrique Fernanadez Alejandria <walter998850@gmail.com>
2020-05-04 17:14:50 +0000

    walter, prueba

--- webapp/controller/Index.controller.js
+++ webapp/controller/Index.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function (Controller) {
 	"use strict";
-
+	
 	return Controller.extend("ebimList.ListBinding.controller.Index", {
 		onInit: function () {
 		
@@ -12,6 +12,11 @@ sap.ui.define([
 			
 				//this.getOwnerComponent().getRouter().navTo("ordenes");
 				this.getOwnerComponent().getTargets().display("ordenes");
+					
+		},
+		
+		onPress2: function (oEvent){
+			this.getOwnerComponent().getTargets().display("activos");
 		},
 
 		onClick: function () {
---
+++ webapp/controller/activos.controller.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ebimList.ListBinding.controller.activos", {
+
+		onInit: function () {
+			
+		
+			
+		},
+		
+		onNavBack : function () {
+			
+			
+		}
+	
+
+	});
+
+});
