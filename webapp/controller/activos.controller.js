ffb4e46ce75d3c93338aae9ff8b08c47697c4d92
Walter Enrique Fernanadez Alejandria <walter998850@gmail.com>
2020-05-04 22:15:21 +0000

    walter- activos

--- webapp/controller/activos.controller.js
+++ webapp/controller/activos.controller.js
@@ -8,8 +8,27 @@ sap.ui.define([
 		onInit: function () {
 			
 			
-			
 		},
+		
+		
+		onPress3: function(){
+			var sUrl = "/sap/opu/odata/sap/ZGMAO_ODATA_SRV/OM_GetListSet";
+			var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
+			sap.ui.getCore().setModel(oModel);
+			
+			oModel.read("/OM_GetDocumentSet", null, ["&$filter=Zorderid eq '99'"],false, 
+						function(oData, oResponse) {
+							
+							jQuery.sap.require("sap.m.MessageToast");
+							sap.m.MessageToast.show("respuesta exitosa");
+		
+						},
+						function(error){
+							jQuery.sap.require("sap.m.MessageToast");
+						 	sap.m.MessageToast.show("Error!");
+					    });
+		},
+		
 		
 		onNavBack : function () {
 			
