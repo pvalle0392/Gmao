4fae832f9353f1fd6bc4a28ce5eb851c10fa8921
Pool Valle Ramos <pvalle0392@gmail.com>
2020-05-08 02:03:19 +0000

    devPool: Cargar json a lista xml.

--- webapp/controller/activos.controller.js
+++ webapp/controller/activos.controller.js
@@ -6,34 +6,34 @@ sap.ui.define([
 	return Controller.extend("ebimList.ListBinding.controller.activos", {
 
 		onInit: function () {
-			
+			this.getView().byId("equi-list").setVisible(false);
 			
 		},
-		 
-		
 		onPress3: function(){ 
+			
+			this.getView().byId("equi-list").setVisible(true); 
+			var ubicacion = this.byId("ub-tec").getValue();
 			var sUrl = "/sap/opu/odata/sap/ZGMAO_ODATA_SRV/";
 			var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
 			sap.ui.getCore().setModel(oModel);
+			var lista = this.byId("equi-list");
 			
-			oModel.read("/Equi_GetListSet", null, ["&$filter=(ZubTec eq 'EBIM')&$expand=EQUILIST"],false, 
+			oModel.read("/Equi_GetListSet", null, ["&$filter=(ZubTec eq '"+ubicacion+"')&$expand=EQUILIST"],false, 
 						function(oData, oResponse) {
 							var arrayData = oData.results[0].EQUILIST.results;	
 							var jsondata = {items: arrayData };
 							var jsonModel = new sap.ui.model.json.JSONModel();
 							jsonModel.setData(jsondata);
 							console.log(arrayData);
-							jQuery.sap.require("sap.m.MessageToast");
-							sap.m.MessageToast.show("respuesta exitosa");
-		
+							lista.setModel(jsonModel);
+							lista.bindAggregation("items", "/items",
+													new sap.ui.core.ObjectListItem({title: "{Zdescript}", intro:"{Zequnr}", number:"{Zemplaza}"}));
+						
 						},
 						function(error){
-							jQuery.sap.require("sap.m.MessageToast");
-						 	sap.m.MessageToast.show("Error!");
+							console.log("error");
 					    });
 		},
-		
-		
 		onNavBack : function () {
 			
 			
--- webapp/controller/ordenes.controller.js
+++ webapp/controller/ordenes.controller.js
@@ -6,7 +6,6 @@ sap.ui.define([
 	return Controller.extend("ebimList.ListBinding.controller.ordenes", {
 
 		onInit: function () {
-		//	this.byId("order-list").getView().hide();
 		this.getView().byId("order-list").setVisible(false);
 		},
 		
@@ -16,143 +15,73 @@ sap.ui.define([
 		},
 		
 		onSearch: function (event){ 
-			var oCombo = this.byId("order-list");
+			var oList = this.byId("order-list");
 			this.getView().byId("order-list").setVisible(true); 
+			var userRequestBody = [];
+			userRequestBody.push({ZOM_GetList_BodySet: [],NAVOMLIST : []});
 			var dateLow = this.byId("fecha-inicio").getValue();
-			var dateLowArray = dateLow.split("-");
-			var dateLow = dateLowArray[0] + dateLowArray[1] + dateLowArray[2];
+			if (dateLow != '') {
+				var dateLowArray = dateLow.split("-");
+				var sapdateLow = dateLowArray[0] + dateLowArray[1] + dateLowArray[2];
+				var var1 = {
+					"FieldName": "SHOW_DOCS_WITH_FROM_DATE",
+				    "Sign": "I",
+				    "Option": "EQ",
+				    "LowValue": sapdateLow,
+				    "HighValue": ""
+				};
+				userRequestBody[0].ZOM_GetList_BodySet.push(var1);
+			}
+			
 			var dateHigh = this.byId("fecha-fin").getValue();
-			var dateHighArray = dateHigh.split("-");
-			var dateHigh = dateHighArray[0] + dateHighArray[1] + dateHighArray[2];
+			if (dateHigh != '') {
+				var dateHighArray = dateHigh.split("-");
+				var sapdateHigh = dateHighArray[0] + dateHighArray[1] + dateHighArray[2];
+				var var2 = {
+					"FieldName": "SHOW_DOCS_WITH_TO_DATE",
+			        "Sign": "I",
+			        "Option": "EQ",
+			        "LowValue": sapdateHigh,
+			        "HighValue": ""
+				};
+				userRequestBody[0].ZOM_GetList_BodySet.push(var2);
+			}
 			var plant = this.byId("plan-plant").getValue();
-			//jQuery.sap.require("sap.m.MessageToast"); 
-			//sap.m.MessageToast.show(dateLow + "//" + dateHigh + "//" + plant);
-		//var oTable  = this.byId("oTable");
+			if (plant != '') {
+				var var3 = {
+					"FieldName": "OPTIONS_FOR_PLANPLANT",
+			        "Sign": "I",
+			        "Option": "EQ",
+			        "LowValue": "GREB",
+			        "HighValue": ""
+				};
+				userRequestBody[0].ZOM_GetList_BodySet.push(var3);
+			}
+			var var4 = {
+				    "Nro": "",
+				    "OrderId": "",
+				    "OrderDesc": ""
+				 };
+			userRequestBody[0].NAVOMLIST.push(var4);
+			//console.log(userRequestBody);
 			var sUrl = "/sap/opu/odata/sap/ZGMAO_ODATA_SRV/";
 			var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
 			sap.ui.getCore().setModel(oModel);
-			var userRequestBody = {
-			
-  "ZOM_GetList_BodySet": [
-    {
-      "FieldName": "SHOW_DOCS_WITH_FROM_DATE",
-      "Sign": "I",
-      "Option": "EQ",
-      "LowValue": "20200316",
-      "HighValue": ""
-    },
-    {
-      "FieldName": "SHOW_DOCS_WITH_TO_DATE",
-      "Sign": "I",
-      "Option": "EQ",
-      "LowValue": "20200330",
-      "HighValue": ""
-    }
-  ],
-  "NAVOMLIST": [
-    {
-      "Nro": "",
-      "OrderId": "",
-      "OrderDesc": ""
-    }
-  ]/*
-						"FieldName" : "",
-					    "Sign" : "",
-					    "Option" : "",
-					    "LowValue" : "",
-					    "HighValue" : "",
-						ZOM_GetList_BodySet : [],
-						NAVOMLIST : []*/
-				
-			};
-		/*	var var1 = {
-				  "FieldName": "SHOW_DOCS_WITH_FROM_DATE",
-			      "Sign": "I",
-			      "Option": "EQ",
-			      "LowValue": "20200316",
-			      "HighValue": ""
-			};
-			var var2 = {
-				"FieldName": "SHOW_DOCS_WITH_FROM_DATE",
-		        "Sign": "I",
-		        "Option": "EQ",
-		        "LowValue": "20200330",
-		        "HighValue": ""
-			};
-			/*var var3 = {
-		 		"Nro": "",
-		        "OrderId": "",
-		        "OrderDesc": ""
-			};*/
-			//userRequestBody.ZOM_GetList_BodySet.push(var1);
-			//userRequestBody.ZOM_GetList_BodySet.push(var2);
-		//	userRequestBody.NAVOMLIST.push(var3);
-			oModel.create("/OM_GetListSet",userRequestBody,null,
+			oModel.create("/OM_GetListSet",userRequestBody[0],null,
 							function(oData, oResponse){
 						    var arrayData = oData.NAVOMLIST.results;	
 							var jsondata = {items: arrayData };
 							var jsonModel = new sap.ui.model.json.JSONModel();
 							jsonModel.setData(jsondata);
-					        console.log(arrayData);
-						    
-							oCombo.setModel(jsonModel);
-							oCombo.bindAggregation("items", "/items",new sap.ui.core.ListItem({text: "{OrdeDesc}", id:"{OrderId}"}));
-								
+							oList.setModel(jsonModel);
+							oList.bindAggregation("items", "/items",
+													new sap.ui.core.ObjectListItem({title: "{OrderDesc}", intro:"{OrderId}"}));
 						    },
 						    function(err){
-						        console.log("error");
+						       // console.log("error");
 						    }
-			);							  
-		
-							
+			);							 
 		}
-		
-
 	});
-
 });
-/*function(oData, oResponse) {
-				
-				var arrayData = oData.results[0].NAVEQUILIST.results;	
-				var jsondata = {items: arrayData };
-				var jsonModel = new sap.ui.model.json.JSONModel();
-				jsonModel.setData(jsondata);
-				
-				console.log(arrayData);
-				
-				var oCombo = that.byId("cboEqui");
-				oCombo.setModel(jsonModel);
-				oCombo.bindAggregation("items", "/items", 
-									new sap.ui.core.ListItem(
-										{ text: "{Zdescript}",
-										  id:"{Zequnr}"
-										}
-									)
-								);
-
-				//oTable.bindRows();
-			}
-			,function(error){
-				//Agregar logica cuando hay error
-				
-			}
-						oModel.create("/OM_GetListSet",userRequestBody,null,{
-							success: function(oData, oResponse){
-						    var arrayData = oData.NAVOMLIST[0];	
-							var jsondata = {items: arrayData };
-							var jsonModel = new sap.ui.model.json.JSONModel();
-							jsonModel.setData(jsondata);
-					        console.log(arrayData);
-						    /*var oCombo = this.byId("order-list");
-							oCombo.setModel(jsonModel);
-							oCombo.bindAggregation("items", "/items",new sap.ui.core.ListItem({text: "{OrdeDesc}", id:"{OrderId}"}));
-								
-						   *//* },
-						    error: function(err){
-						        console.log("error");
-						    }
-			});	
-			
-			
-			*/
 
